0e745b287760b35b6e9764f58ab126f8954d4abe
Jaime Florian <jaimeflorian27@gmail.com>
2023-06-07 22:27:21 -0700

    added aerial.nvim

--- after/plugin/_aerial.nvim.lua
+++ after/plugin/_aerial.nvim.lua
@@ -8,3 +8,8 @@ require('aerial').setup({
 })
 -- You probably also want to set a keymap to toggle aerial
 vim.keymap.set('n', '<leader>a', '<cmd>AerialToggle!<CR>')
+
+require('telescope').load_extension('aerial')
+
+vim.keymap.set('n', '<leader>fs', ':Telescope aerial<CR>', {buffer = bufnr})
+vim.keymap.set('n', '<leader>an', ':AerialNavToggle<CR>', { noremap=true, silent=true })
--- after/plugin/_lualine.lua
+++ after/plugin/_lualine.lua
@@ -23,7 +23,8 @@ require('lualine').setup {
     lualine_c = {'filename'},
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
-    lualine_z = {'location'}
+    lualine_z = {'location'},
+    lualine_x = { "aerial" },
   },
   inactive_sections = {
     lualine_a = {},
